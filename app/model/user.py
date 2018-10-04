412efd411bb3c10d1311ea898990b5b643c5f590
SelaDanti <kwameasiago@gmail.com>
2018-10-04 21:53:33 +0300

    :new: [Feature #160887043] Add methods to Verify

--- app/model/user.py
+++ app/model/user.py
@@ -1,2 +1,16 @@
-class user:
-	pass
+from .verify import Verify
+
+
+class user(Verify):
+	def __init__(self,first_name,last_name,email,password,role):
+		self.first_name = first_name
+		self.last_name = last_name
+		self.email = email
+		self.password = password
+		self.role = role
+
+	def add_user(self):
+		pass
+
+	def remove_user(self,id):
+		pass
--- app/model/verify.py
+++ app/model/verify.py
@@ -1,2 +1,33 @@
-class Verify:
-	pass
+import re
+
+class Verifications:
+	"""
+	class to verify data before inserting to database
+	"""
+	def __init__(self):
+		self.data_err = None
+
+	# check if input is empty
+	def is_empty(self,items):
+		for item in items:
+			if not item:
+				self.error = {'result': 'Invalid input(Empty data)'}, 405
+				return self.error
+		return False
+
+	# check if input contains only space
+	def is_space(self,items):
+		for item in items:
+			if item.isspace() is True:
+				self.error = {'result': 'Invalid input(Whitespace data)'}, 405
+				return self.error
+		return False
+
+	# check if email input is empty
+	def is_email(self,email):
+		result = re.match('^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$', email)
+		if result is None:
+			self.error = {'result': 'Invalid email'}, 405
+			return self.error
+		else:
+			return False
